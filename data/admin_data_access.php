4690a551f26472b018dbccf6dd7222060b623f7a
Arefin <rafin.ryan.07@outlook.com>
2017-12-14 14:32:43 +0600

    php

---
+++ data/admin_data_access.php
@@ -0,0 +1,25 @@
+<?php require_once "data_access.php"; ?>
+
+<?php
+
+	function createEmployeeDB($employee){
+		
+	}
+	
+	function deleteEmployeeDB($id){
+		
+	}
+	
+	function updateEmployeeDB($id){
+		
+	}
+	
+	function getAllEmployeeDB(){
+		
+	}
+	
+	function getEmployeeByIdDB($id){
+		
+	}
+
+?>
